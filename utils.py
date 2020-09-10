d760d6cbff22e1b9dff62bf23ca19e5b329dba67
Marco Gelli <marcogelli1994@gmail.com>
2020-09-10 14:54:43 +0200

    Added new problems

--- utils.py
+++ utils.py
@@ -1,3 +1,25 @@
+import math
+
+def divisors(n):
+    divs = []
+    d = 1
+    while d <= math.sqrt(n):
+        if n % d == 0:
+            if n / d == d:
+                divs.append(d)
+            else:
+                divs.append(d)
+                divs.append(int(n / d))
+        d += 1
+    return divs
+
+
+def proper_divisors(n):
+    divs = divisors(n)
+    divs.remove(n)
+    return divs
+
+
 def binom(n, k): 
     """
     Computes the binomial coefficient C(n, k), the number of combinations of n
