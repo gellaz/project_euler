11eba169d7c4aa2042bf952a43d6782cf49b9739
Marco Gelli <marcogelli1994@gmail.com>
2021-08-07 21:26:42 +0200

    Updated utils with prime functions and added solution to p031

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import math
 
+
 def divisors(n):
     divs = []
     d = 1
+
     while d <= math.sqrt(n):
         if n % d == 0:
             if n / d == d:
@@ -11,6 +13,7 @@ def divisors(n):
                 divs.append(d)
                 divs.append(int(n / d))
         d += 1
+
     return divs
 
 
@@ -20,7 +23,7 @@ def proper_divisors(n):
     return divs
 
 
-def binom(n, k): 
+def binom(n: int, k: int) -> int:
     """
     Computes the binomial coefficient C(n, k), the number of combinations of n
     things taken k at a time.
@@ -40,11 +43,27 @@ def binom(n, k):
     """
     if not 0 <= k <= n:
         return 0
-    
-    b=1
-    for t in range(min(k, n-k)):
+
+    b = 1
+
+    for t in range(min(k, n - k)):
         b *= n
-        b /= t+1
+        b /= t + 1
         n -= 1
 
     return int(b)
+
+
+def is_prime(num: int) -> bool:
+    if num > 1:
+        for i in range(2, num // 2):
+            if (num % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def primes_range(l_bound, u_bound):
+    return [n for n in range(l_bound, u_bound + 1) if is_prime(n)]
